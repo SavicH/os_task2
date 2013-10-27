16fa8ab9862adc5fdb42853a8eb3b4ea39ca1ded
Sergei <savich@home.(none)>
2013-10-27 16:05:07 +0400

    Server: non-blocking lister

--- server.c
+++ server.c
@@ -7,7 +7,52 @@
 #include "pop3.h"
 
 #define PORT 2558
+#define MAXEVENTS 32
 #define FILENAME "data.txt"
+
+static int make_socket_non_blocking (int sfd)
+{
+    int flags, s;
+
+    flags = fcntl (sfd, F_GETFL, 0);
+    if (flags == -1)
+    {
+        perror ("fcntl");
+        return -1;
+    }
+
+    flags |= O_NONBLOCK;
+    s = fcntl (sfd, F_SETFL, flags);
+    if (s == -1)
+    {
+        perror ("fcntl");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int create_listener(int *listener)
+{
+    struct sockaddr_in addr;
+
+    *listener = socket(AF_INET, SOCK_STREAM, 0);
+    if(listener < 0)
+    {
+        perror("socket");
+        exit(2);
+    }
+    
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if(bind(*listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind");
+        exit(3);
+    }
+}
+
 
 int main()
 {
@@ -19,30 +64,16 @@ int main()
         exit(1);
     }
 
-    int sock, listener;
-    struct sockaddr_in addr;
+    int listener;
+    create_listener(&listener);
 
-    listener = socket(AF_INET, SOCK_STREAM, 0);
-    if(listener < 0)
-    {
-        perror("socket");
-        exit(2);
-    }
-    
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    {
-        perror("bind");
-        exit(3);
-    }
+    make_socket_non_blocking(listener);
 
     listen(listener, 5);
     
     while(1)
     {
-        sock = accept(listener, NULL, NULL);
+        int sock = accept(listener, NULL, NULL);
         if(sock < 0)
         {
             perror("accept");
